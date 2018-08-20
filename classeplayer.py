89fb1ea577e95658d6cad45f985eae0868477460
bryan <bryanroger@live.com>
2018-08-20 19:27:55 -0300

    atualização

--- classeplayer.py
+++ classeplayer.py
@@ -8,7 +8,7 @@ class player():
         inteligencia = 4
         defesa = 2
         atk = ((força / 2 + magiap * 2) / 2) + magiap * 0.15 + força * 0.05
-
+        #6.5
     class guerreiro:
         nome = ('Guerreiro')
         vida = 8
@@ -17,16 +17,16 @@ class player():
         defesa = 3
         inteligencia = 1.5
         atk = ((força * 2 + magiap / 2) / 2) + força * 0.1 + magiap * 0.05
-
-    class Amazona:
+        5.7
+    class amazona:
         nome = ('Amazona')
         vida = 7
         magiap = 2.5
         força = 3.5
         defesa = 3.5
         inteligencia = 2.5
-
-
+        atk = ((força * 2 + (magiap + inteligencia*2)/2) / 2) + força * 0.05 + magiap * 0.05 + inteligencia * 0.05
+        5.8
     class nifrin:
         nome = ('Nifrin')
         vida = 6.5
@@ -34,3 +34,5 @@ class player():
         força = 3
         defesa = 3.5
         inteligencia = 2.5
+        atk = ((força + magiap + inteligencia) / 2) + força * 0.1 + magiap * 0.05 + inteligencia * 0.05
+        #5.375
--- novopersonagem.py
+++ novopersonagem.py
@@ -1,12 +1,19 @@
 import classeplayer
 esnome = input('Escolha seu nome: ')
 classe = input('Olá {}, dentre as classes: Mago, Guerreiro, Nefrin e Amazona, qual você escolhe?'.format(esnome))
+classe.lower()
 if classe == ('mago'):      
     player = classeplayer.player.mago
     print('Mago tem vida inicial de: ', classeplayer.player.mago.vida)
 elif classe == ('guerreiro'):
     player = classeplayer.player.guerreiro
     print(classeplayer.player.guerreiro)
+elif classe == ('amazona'):
+    player = classeplayer.player.amazona
+    print(classeplayer.player.amazona)
+elif classe == ('nifrin'):
+    player = classeplayer.player.nifrin
+    print(classeplayer.player.nifrin)
 else:
     print('Classe não conhecida, escolha uma classe valida: ')
     classe = input(' ')
