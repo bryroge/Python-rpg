f888b38fd11054fa444efc298881d114c90d867e
bryan <bryanroger@live.com>
2018-08-20 18:31:25 -0300

    atualização

--- classemonstros.py
+++ classemonstros.py
@@ -1,8 +1,14 @@
 class monsters():
     class orc:
-        vidain = 10
-        atqin = 5
+        vidain = 50
+        força = 5
+        magiain = 5
+        defesa = 8
+        defe = defesa / 2 * força / 2
 
-    class dragon:
-        vidain = 10
-        atqin = 5
+    class dragão:
+        vidain = 60
+        força = 8
+        magiain = 13
+        defesa = 12
+        defe = defesa / 2 * força / 2
