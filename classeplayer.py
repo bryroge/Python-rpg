76387152feeb3679a16a589990e61587e587252d
bryan <bryanroger@live.com>
2018-08-20 19:37:31 -0300

    atualização

--- classeplayer.py
+++ classeplayer.py
@@ -11,7 +11,7 @@ class player():
         #6.5
     class guerreiro:
         nome = ('Guerreiro')
-        vida = 8
+        vida = 8.5
         magiap = 2.5
         força = 4.5
         defesa = 3
@@ -20,19 +20,19 @@ class player():
         5.7
     class amazona:
         nome = ('Amazona')
-        vida = 7
+        vida = 6.5
         magiap = 2.5
         força = 3.5
         defesa = 3.5
-        inteligencia = 2.5
+        inteligencia = 3
         atk = ((força * 2 + (magiap + inteligencia*2)/2) / 2) + força * 0.05 + magiap * 0.05 + inteligencia * 0.05
-        5.8
+        #6.225
     class nifrin:
         nome = ('Nifrin')
-        vida = 6.5
-        magiap = 4
-        força = 3
+        vida = 7
+        magiap = 3.5
+        força = 4
         defesa = 3.5
-        inteligencia = 2.5
+        inteligencia = 3
         atk = ((força + magiap + inteligencia) / 2) + força * 0.1 + magiap * 0.05 + inteligencia * 0.05
-        #5.375
+        #6.25
